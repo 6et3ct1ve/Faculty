0765f03da7f92aa73d949bdb45f5d5ee53cb3bd6
6et3ct1ve <6et3ct1ve@gmail.com>
2025-09-23 10:39:33 +0300

    Forget create repository in start

--- FNS/admin.py
+++ FNS/admin.py
@@ -1,2 +1,27 @@
 from django.contrib import admin
+from .models import Department, Program, Teacher, HomePage
 
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ["name", "head"]
+    search_fields = ["name"]
+
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ["code", "name", "department", "coordinator_name"]
+    list_filter = ["department"]
+    search_fields = ["name", "code"]
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ["name", "position", "degree", "department"]
+    list_filter = ["department", "position"]
+    search_fields = ["name"]
+
+
+@admin.register(HomePage)
+class HomePageAdmin(admin.ModelAdmin):
+    list_display = ["title"]
--- FNS/urls.py
+++ FNS/urls.py
@@ -1,2 +1,16 @@
 from django.urls import path
+from . import views
 
+app_name = "FNS"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("programs/", views.programs_list, name="programs_list"),
+    path("programs/<int:program_id>/", views.program_detail, name="program_detail"),
+    path("departments/", views.departments_list, name="departments_list"),
+    path(
+        "departments/<int:department_id>/",
+        views.department_detail,
+        name="department_detail",
+    ),
+]
